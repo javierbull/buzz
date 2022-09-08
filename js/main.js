136d5f8d4c38e69a2ad39531b19b15cee9a5c6b6
javierbull <javierbullreyes@gmail.com>
2022-09-08 10:56:41 -0500

    agrego estado y quito checkboxes del panel envios pendientes

--- js/main.js
+++ js/main.js
@@ -66,28 +66,28 @@ $('.dtable').DataTable({
 
 });
 
-$('#tableEnviosPendientes').DataTable({
-  "language": {
-    "lengthMenu": "Mostrar _MENU_ filas",
-    "zeroRecords": "No se encontraron resultados",
-    "info": "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
-    "infoEmpty": "Mostrando registros del 0 al 0 de un total de 0 registros",
-    "infoFiltered": "(filtrado de un total de _MAX_ registros)",
-    "sSearch": "Buscar:",
-    "oPaginate": {
-      "sFirst": "Primero",
-      "sLast": "Último",
-      "sNext": "Siguiente",
-      "sPrevious": "Anterior"
-    },
-    "sProcessing": "Procesando...",
-  },
-  "columnDefs": [{
-    "orderable": false,
-    "targets": 0
-  }],
-  "order": [], // not set any order rule for any column.
-});
+// $('#tableEnviosPendientes').DataTable({
+//   "language": {
+//     "lengthMenu": "Mostrar _MENU_ filas",
+//     "zeroRecords": "No se encontraron resultados",
+//     "info": "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
+//     "infoEmpty": "Mostrando registros del 0 al 0 de un total de 0 registros",
+//     "infoFiltered": "(filtrado de un total de _MAX_ registros)",
+//     "sSearch": "Buscar:",
+//     "oPaginate": {
+//       "sFirst": "Primero",
+//       "sLast": "Último",
+//       "sNext": "Siguiente",
+//       "sPrevious": "Anterior"
+//     },
+//     "sProcessing": "Procesando...",
+//   },
+//   "columnDefs": [{
+//     "orderable": false,
+//     "targets": 0
+//   }],
+//   "order": [],
+// });
 
 
 // End datatables
