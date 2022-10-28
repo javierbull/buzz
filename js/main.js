eab0405ea1b39cfb927234224312370433ea9311
Javier Bull <javierbullreyes@gmail.com>
2022-10-28 16:34:51 -0500

    rich text editor toolbar

--- js/main.js
+++ js/main.js
@@ -398,7 +398,7 @@ $('#selectCanalVentaModal').on('change', function(){
 tinymce.init({
   selector: "richtextarea",
   plugins: "lists advlist autolink autoresize charmap code emoticons hr image insertdatetime link media paste preview searchreplace table textpattern toc visualblocks visualchars wordcount quickbars",
-  toolbar: "code preview | undo redo | formatselect | fontselect | fontsizeselect | bold italic underline strikethrough backcolor | subscript superscript | numlist bullist | alignleft aligncenter alignright alignjustify | outdent indent | paste searchreplace | toc link image media charmap insertdatetime emoticons hr | table tabledelete | tableprops tablerowprops tablecellprops | tableinsertrowbefore tableinsertrowafter tabledeleterow | tableinsertcolbefore tableinsertcolafter tabledeletecol | removeformat",
+  toolbar: "preview | undo redo | fontsizeselect | bold italic underline strikethrough backcolor | numlist bullist | alignleft aligncenter alignright alignjustify | outdent indent | paste | link image charmap emoticons hr | table tabledelete",
   insertdatetime_element: true,
    media_scripts: [
    {filter: 'platform.twitter.com'},
