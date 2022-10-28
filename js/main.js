69ec47790872dcc711ca2f7284d2b8eac9223e43
Javier Bull <javierbullreyes@gmail.com>
2022-10-28 13:43:02 -0500

    select 2 multiple full js

--- js/main.js
+++ js/main.js
@@ -339,7 +339,7 @@ $('.select2modal').each(function() {
   });
 });
 
-$( '.select2-multiple' ).select2( {
+$( '#select2-multiple' ).select2( {
     theme: "bootstrap-5",
     width: $( this ).data( 'width' ) ? $( this ).data( 'width' ) : $( this ).hasClass( 'w-100' ) ? '100%' : 'style',
     placeholder: $( this ).data( 'placeholder' ),
