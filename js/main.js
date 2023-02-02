aee7dd9830c1c7abb61f16394d3472663d2e5b5c
javierbull <javierbullreyes@gmail.com>
2023-02-02 11:43:40 -0500

    nueva empresa img preview logo

--- js/main.js
+++ js/main.js
@@ -921,6 +921,21 @@ $('#descarga-vales-input-preview-img').change(function(){
     reader.readAsDataURL(this.files[0]);
 });
 
+// image preview logo empresas
+$('#nueva-empresa-input-preview-img').change(function(){
+    var curElement = $('#nueva-empresa-preview-logo-empresa');
+    console.log(curElement);
+    var reader = new FileReader();
+
+    reader.onload = function (e) {
+        // get loaded data and render thumbnail.
+        curElement.attr('src', e.target.result);
+    };
+
+    // read the image file as a data URL.
+    reader.readAsDataURL(this.files[0]);
+});
+
 // END FILE UPLOAD IMG
 
 
